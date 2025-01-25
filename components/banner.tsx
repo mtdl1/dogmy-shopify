02fb313d2150a5c7a34f17394ab53669ab3377b3
Mateus Dev <mateusdubovicky@hotmail.com>
2025-01-25 01:01:46 -0300

    Teste Banner com Imagem

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
+
 // [components/banner.tsx](components/banner.tsx)
 export default function Banner() {
   return (
     <div className="bg-blue-500 text-white p-4">
-      Banner de promoção!
+      <Image src="/banner.jpg" alt="Banner" width={2676} height={1016} />
     </div>
   );
 }

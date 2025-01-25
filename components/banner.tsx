995d18e208f5150ab37f3eaaff969988ae0465c7
Mateus Dev <mateusdubovicky@hotmail.com>
2025-01-25 00:35:27 -0300

    Teste Banner

---
+++ components/banner.tsx
@@ -0,0 +1,8 @@
+// [components/banner.tsx](components/banner.tsx)
+export default function Banner() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      Banner de promoção!
+    </div>
+  );
+}

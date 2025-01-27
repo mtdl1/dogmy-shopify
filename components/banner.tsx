861d374eef6225fc13748640796a293d75d7909f
Mateus Dev <mateusdubovicky@hotmail.com>
2025-01-27 10:41:44 -0300

    Banner Responsivo

--- components/banner.tsx
+++ components/banner.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 export default function Banner() {
   return (
     <div className="mx-auto max-w-screen-2xl px-4 pb-4 md:grid-cols-6 rounded-lg">
-      <Image className="rounded-lg" src="/banner_site_A1.jpg" alt="Banner" width={2720} height={1275} />
+      <Image className="rounded-lg hidden md:block" src="/banner_site_A1.jpg" alt="Banner" width={2720} height={1275} />
+      <Image className="rounded-lg block md:hidden" src="/banner_site_mobile.jpg" alt="Banner" width={500} height={610} />
     </div>
   );
 }

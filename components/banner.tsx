363489a281748b8cda00ccb1e4e59157bff894fc
Mateus Dev <mateusdubovicky@hotmail.com>
2025-01-27 09:08:20 -0300

    Banner, Estilização Clara, Traduções e alguns ajustes

--- components/banner.tsx
+++ components/banner.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 // [components/banner.tsx](components/banner.tsx)
 export default function Banner() {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      <Image src="/banner.jpg" alt="Banner" width={2676} height={1016} />
+    <div className="mx-auto max-w-screen-2xl px-4 pb-4 md:grid-cols-6 rounded-lg">
+      <Image className="rounded-lg" src="/banner_site_A1.jpg" alt="Banner" width={2720} height={1275} />
     </div>
   );
 }
--- components/cart/modal.tsx
+++ components/cart/modal.tsx
@@ -76,7 +76,7 @@ export default function CartModal() {
           >
             <Dialog.Panel className="fixed bottom-0 right-0 top-0 flex h-full w-full flex-col border-l border-neutral-200 bg-white/80 p-6 text-black backdrop-blur-xl md:w-[390px] dark:border-neutral-700 dark:bg-black/80 dark:text-white">
               <div className="flex items-center justify-between">
-                <p className="text-lg font-semibold">My Cart</p>
+                <p className="text-lg font-semibold">Meu Carrinho</p>
                 <button aria-label="Close cart" onClick={closeCart}>
                   <CloseCart />
                 </button>
@@ -85,7 +85,7 @@ export default function CartModal() {
               {!cart || cart.lines.length === 0 ? (
                 <div className="mt-20 flex w-full flex-col items-center justify-center overflow-hidden">
                   <ShoppingCartIcon className="h-16" />
-                  <p className="mt-6 text-center text-2xl font-bold">Your cart is empty.</p>
+                  <p className="mt-6 text-center text-2xl font-bold">Seu carrinho está vazio</p>
                 </div>
               ) : (
                 <div className="flex h-full flex-col justify-between overflow-hidden p-1">

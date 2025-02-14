2ad5dbd18fa5e80e7a0c80a60e4b6da736bb2a72
Mateus Dev <mateusdubovicky@hotmail.com>
2025-02-14 14:10:23 -0300

    Banner full

--- components/banner.tsx
+++ components/banner.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 // [components/banner.tsx](components/banner.tsx)
 export default function Banner() {
   return (
-    <div className="mx-auto max-w-screen-2xl px-4 pb-4 md:grid-cols-6 rounded-lg">
-      <Image className="rounded-lg hidden md:block" src="/banner_site_A1.jpg" alt="Banner" width={2720} height={1275} />
-      <Image className="rounded-lg block md:hidden" src="/banner_site_mobile.jpg" alt="Banner" width={500} height={610} />
+    <div className="mx-auto pb-4 md:grid-cols-6">
+      <Image className="hidden md:block" src="/banner_site_A1.jpg" alt="Banner" width={2720} height={1275} />
+      <Image className="block md:hidden" src="/banner_site_mobile.jpg" alt="Banner" width={500} height={610} />
     </div>
   );
 }

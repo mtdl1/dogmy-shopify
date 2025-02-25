8ad264aad149e7527bae0a7db0b57bd080b90f5a
Mateus Dev <mateusdubovicky@hotmail.com>
2025-02-24 21:56:53 -0300

    Add to Cart para COmpre Agora e mais traducoes

--- components/cart/add-to-cart.tsx
+++ components/cart/add-to-cart.tsx
@@ -1,6 +1,4 @@
 'use client';
-
-import { PlusIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import { addItem } from 'components/cart/actions';
 import { useProduct } from 'components/product/product-context';
@@ -16,13 +14,13 @@ function SubmitButton({
   selectedVariantId: string | undefined;
 }) {
   const buttonClasses =
-    'relative flex w-full items-center justify-center rounded-full bg-blue-600 p-4 tracking-wide text-white';
+    'relative flex w-full items-center justify-center rounded-full bg-green-600 p-4 tracking-wide text-white';
   const disabledClasses = 'cursor-not-allowed opacity-60 hover:opacity-60';
 
   if (!availableForSale) {
     return (
       <button disabled className={clsx(buttonClasses, disabledClasses)}>
-        Out Of Stock
+        PRODUTO ESGOTADO
       </button>
     );
   }
@@ -35,10 +33,7 @@ function SubmitButton({
         disabled
         className={clsx(buttonClasses, disabledClasses)}
       >
-        <div className="absolute left-0 ml-4">
-          <PlusIcon className="h-5" />
-        </div>
-        Add To Cart
+        SELECIONE UMA OPÇÃO
       </button>
     );
   }
@@ -50,10 +45,7 @@ function SubmitButton({
         'hover:opacity-90': true
       })}
     >
-      <div className="absolute left-0 ml-4">
-        <PlusIcon className="h-5" />
-      </div>
-      Add To Cart
+      COMPRE AGORA
     </button>
   );
 }
